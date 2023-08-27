d11836dbb8596bfffc9dc2990a267a8a0829022b
Ravish Kumar <117345692+Ravish3389@users.noreply.github.com>
2023-08-27 15:24:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,27 +3,32 @@ const mongoose = require('mongoose');
 const mathRoutes = require('./routes/math');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-async function connection(){
-    try{
-        await mongoose.connect('mongodb+srv://kr9532:Chinni0107@cluster0.1zi7bon.mongodb.net/Kalvium');
-        console.log("Database connected sucessfully");
-    }
-    catch(err){
+async function connectToDatabase() {
+    try {
+        await mongoose.connect('mongodb+srv://kr9532:Chinni0107@cluster0.1zi7bon.mongodb.net/Kalvium', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Database connected successfully");
+    } catch (err) {
         console.error(err);
     }
 }
-  
-connection();
 
+connectToDatabase();
 
 app.use('/', mathRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
 });
 
-
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
